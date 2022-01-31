e8f335219af734e34ed6f789552d92bd990717d1
twelwet <akimovmax@mail.ru>
2022-01-31 20:31:30 +0300

    prepare initial data to bill in data/json

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const FilePath = {
+  Spot: {
+    CSV: `data/csv/spot-trades-actual.csv`,
+    JSON: `data/json/spot-trades-actual.json`,
+  },
+  FuturesUsdM: {
+    CSV: `data/csv/futures-trades-actual.csv`,
+    JSON: `data/json/futures-trades-actual.json`,
+  },
+  FuturesCoinM: {
+    CSV: `data/csv/futures-trades-coin-m-actual.csv`,
+    JSON: `data/json/futures-trades-coin-m-actual.json`,
+  },
+};
+
+module.exports = {FilePath};
---
+++ src/scripts/prepare-initial-data.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const {saveToFile, getJsonFromCsv} = require(`../utils`);
+const {FilePath} = require(`../constants`);
+
+const prepareJson = (pathToCsvFile, resultPath) => {
+  getJsonFromCsv(pathToCsvFile)
+    .then((data) => saveToFile(resultPath, JSON.stringify(data)));
+};
+
+prepareJson(FilePath.Spot.CSV, FilePath.Spot.JSON);
+prepareJson(FilePath.FuturesUsdM.CSV, FilePath.FuturesUsdM.JSON);
+prepareJson(FilePath.FuturesCoinM.CSV, FilePath.FuturesCoinM.JSON);
