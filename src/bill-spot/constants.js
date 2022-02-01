20beb5ac5b0231225abc6200dd14da186828c64e
twelwet <akimovmax@mail.ru>
2022-02-01 17:34:56 +0300

    add BaseCoin to constants

--- src/bill-spot/constants.js
+++ src/bill-spot/constants.js
@@ -3,5 +3,6 @@
 const SPOT_SIGNAL_CATEGORY = `spot-signal`;
 const Type = {BUY: `BUY`, SELL: `SELL`};
 const TradeType = {OPENED: `opened`, CLOSED: `closed`};
+const BaseCoin = {USDT: `USDT`, BTC: `BTC`};
 
-module.exports = {Type, TradeType, SPOT_SIGNAL_CATEGORY};
+module.exports = {Type, TradeType, SPOT_SIGNAL_CATEGORY, BaseCoin};
