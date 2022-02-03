84d516ae944e0857dfbf9443e632970bdbd552e1
twelwet <akimovmax@mail.ru>
2022-02-03 12:21:01 +0300

    add Fee column

--- src/bill-f-usdt/utils.js
+++ src/bill-f-usdt/utils.js
@@ -17,6 +17,8 @@ const getCountable = (data) => {
 
 const getAssetProfit = (trades) => trades.length > 0 ? trades.map((asset) => asset[`realizedPnl`]).reduce(reducer) : 0;
 
+const getFee = (trades) => trades.length > 0 ? trades.map((asset) => asset[`fee`]).reduce(reducer) : 0;
+
 const getIsClosed = (coinsBuy, coinsSell) => +((coinsBuy - coinsSell).toFixed(5)) <= +(2 * (coinsBuy / 1000).toFixed(5));
 
 const getBilling = (allTrades, categoryName) => {
@@ -24,13 +26,13 @@ const getBilling = (allTrades, categoryName) => {
   const assets = [...new Set(trades.map((item) => item[`symbol`]))];
   const result = [];
 
-
   for (const asset of assets) {
     const assetTrades = trades.filter((item) => item[`symbol`] === asset);
     const {buyCoins, sellCoins} = getAssetInfo(assetTrades);
     const profit = getAssetProfit(assetTrades);
+    const fee = getFee(assetTrades);
     const isClosed = getIsClosed(buyCoins, sellCoins);
-    result.push({asset, buyCoins, sellCoins, profit, isClosed});
+    result.push({asset, buyCoins, sellCoins, isClosed, profit, fee});
   }
 
   return result;
