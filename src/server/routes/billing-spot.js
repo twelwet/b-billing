14060d902dcd4e50e95ccfb4cdd5d04232bf5f66
twelwet <akimovmax@mail.ru>
2022-02-04 18:13:46 +0300

    refactor spot-routes

--- src/server/routes/billing-spot.js
+++ src/server/routes/billing-spot.js
@@ -6,43 +6,16 @@ const {Category, BaseCoin} = require(`../../bill/constants`);
 
 const billingSpotRouter = new Router();
 
-billingSpotRouter.get(`/signal/${BaseCoin.BTC}`, (req, res) => {
-  const {pairs, summaryInfo, generalInfo} = getSpotData(Category.Spot.SIGNAL, BaseCoin.BTC);
+billingSpotRouter.get(`/:category/:baseCoin`, (req, res) => {
+  const category = req.params[`category`];
+  const baseCoin = req.params[`baseCoin`];
+  const {pairs, summaryInfo, generalInfo} = getSpotData(category, baseCoin);
   const pageContent = {
     title: generalInfo.name,
-    pairs,
-    summaryInfo,
-    generalInfo,
-  };
-  res.render(`spot`, pageContent);
-});
-
-billingSpotRouter.get(`/signal/${BaseCoin.USDT}`, (req, res) => {
-  const {pairs, summaryInfo, generalInfo} = getSpotData(Category.Spot.SIGNAL, BaseCoin.USDT);
-  const pageContent = {
-    title: generalInfo.name,
-    pairs,
-    summaryInfo,
-    generalInfo,
-  };
-  res.render(`spot`, pageContent);
-});
-
-billingSpotRouter.get(`/classic/${BaseCoin.BTC}`, (req, res) => {
-  const {pairs, summaryInfo, generalInfo} = getSpotData(Category.Spot.CLASSIC, BaseCoin.BTC);
-  const pageContent = {
-    title: generalInfo.name,
-    pairs,
-    summaryInfo,
-    generalInfo,
-  };
-  res.render(`spot`, pageContent);
-});
-
-billingSpotRouter.get(`/classic/${BaseCoin.USDT}`, (req, res) => {
-  const {pairs, summaryInfo, generalInfo} = getSpotData(Category.Spot.CLASSIC, BaseCoin.USDT);
-  const pageContent = {
-    title: generalInfo.name,
+    categories: {
+      spot: Object.values(Category.Spot),
+    },
+    baseCoins: Object.values(BaseCoin),
     pairs,
     summaryInfo,
     generalInfo,
