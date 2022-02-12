ffd6f7a53e2d5b6cd03315329fd6609d1d3e1538
twelwet <akimovmax@mail.ru>
2022-02-12 15:44:52 +0300

    f-coin-entry-point gets data from data-entry-point

--- src/bill/f-coin/index.js
+++ src/bill/f-coin/index.js
@@ -1,10 +1,7 @@
 'use strict';
 
-const futuresCoinTradesRow = require(`../../../data/json/futures-trades-coin-m-actual.json`);
-const {getCountable} = require(`../bill-utils`);
 const {getBilling} = require(`./utils`);
-
-const futuresCoinTrades = getCountable(futuresCoinTradesRow, [`price`, `amount`, `total`, `realizedPnl`, `fee`, `leverage`, `invest`]);
+const {futuresCoinTrades} = require(`../../data-entry-point`);
 
 const getFuturesCoinData = (categoryName, coin) => {
   const pair = getBilling(futuresCoinTrades, categoryName, coin);
