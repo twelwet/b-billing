327349cdfff958ea3efede1920b22903bc3c2f7b
twelwet <akimovmax@mail.ru>
2022-02-15 19:08:50 +0300

    add script that request historical price of the symbol

---
+++ src/scripts/get-symbol-historical-price.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const moment = require(`moment`);
+const {getHistoricalSymbolPrice} = require(`../services/node-binance-api/methods`);
+
+const SYMBOL = `1INCHBTC`;
+const TIME_STRING = `10.02.2022 00:00:00`;
+
+const timeStamp = moment(TIME_STRING, `DD.MM.YYYY HH:mm:ss`).valueOf();
+
+getHistoricalSymbolPrice(SYMBOL, timeStamp).then((data) => console.log(data));
