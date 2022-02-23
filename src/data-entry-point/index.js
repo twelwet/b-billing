b92a98aca1d97ad411317da5987a86777e8397c9
twelwet <akimovmax@mail.ru>
2022-02-23 20:03:08 +0300

    refactor data-entry-point

--- src/data-entry-point/index.js
+++ src/data-entry-point/index.js
@@ -1,19 +1,11 @@
 'use strict';
 
 const {getCountable} = require(`../bill/bill-utils`);
-const spotTradesRow = require(`../../data/json/spot-trades-actual.json`);
-const mutatedTradesRow = require(`../../data/json/spot-trades-only-mutated.json`);
+const spotTradesMutated = require(`../../data/json/spot-trades-mutated.json`);
 const futuresTradesRow = require(`../../data/json/futures-trades-actual.json`);
 const futuresCoinTradesRow = require(`../../data/json/futures-trades-coin-m-actual.json`);
 
-const getSpotTrades = () => {
-  const spotTradesActual = getCountable(spotTradesRow, [`price`, `amount`, `total`, `fee`, `isShift`]);
-  const mutatedTrades = getCountable(mutatedTradesRow, [`price`, `amount`, `total`, `fee`, `isShift`]);
-  const tradesWithoutMutatedItems = spotTradesActual.filter((trade) => trade[`isShift`] === 0);
-  return tradesWithoutMutatedItems.concat(mutatedTrades);
-};
-
-const spotTrades = getSpotTrades();
+const spotTrades = getCountable(spotTradesMutated, [`price`, `amount`, `total`, `fee`, `isShift`]);
 const futuresTrades = getCountable(futuresTradesRow, [`price`, `amount`, `total`, `realizedPnl`, `fee`, `leverage`, `invest`]);
 const futuresCoinTrades = getCountable(futuresCoinTradesRow, [`price`, `amount`, `total`, `realizedPnl`, `fee`, `leverage`, `invest`]);
 
