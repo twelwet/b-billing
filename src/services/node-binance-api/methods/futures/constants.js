f10a90e00568238ca6c2d3222fa14b5ef3d27940
twelwet <akimovmax@mail.ru>
2022-02-27 20:10:46 +0300

    change getFuturesTrades interval to 'last month' (4 weeks)

--- src/services/node-binance-api/methods/futures/constants.js
+++ src/services/node-binance-api/methods/futures/constants.js
@@ -4,12 +4,12 @@ const MILLISECOND = 1; // one millisecond
 const PERIOD_INTERVAL = 86400 * 1000 * 7; // 1 week in ms
 
 const FuturesTradeParam = {
-  PERIODS: 2, // quantity of periods
+  PERIODS: 4, // quantity of periods
   PERIOD_LIMIT: 1000, // max quantity of symbol trades in one period
 };
 
 const FuturesCoinTradeParam = {
-  PERIODS: 2, // quantity of periods
+  PERIODS: 4, // quantity of periods
   PERIOD_LIMIT: 100, // max quantity of coin-m symbol trades in one period
 };
 
